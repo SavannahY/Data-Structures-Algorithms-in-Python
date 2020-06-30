7d607baae55ef70cefef381f9209b6aef4994a3e
Jane Yang <50490168+SavannahY@users.noreply.github.com>
2020-06-30 11:09:01 -0400

    Update and rename Fibonacci sequence to fibonacci_sequence.py

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -1,6 +1,6 @@
 """Implement a function recursively to get the desired
 Fibonacci sequence value.
-Your code should have the same input/output as the 
+Your code should have the same input output as the 
 iterative code in the instructions."""
 
 def get_fib(position):
