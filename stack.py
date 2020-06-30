9c86edce757069147db349cda115ba81746c9a9c
Jane Yang <50490168+SavannahY@users.noreply.github.com>
2020-06-30 11:05:34 -0400

    Update and rename Stack to stack.py

--- stack.py
+++ stack.py
@@ -2,9 +2,7 @@
 and use that to implement a Stack.
 You have 4 functions below to fill in:
 insert_first, delete_first, push, and pop.
-Think about this while you're implementing:
-why is it easier to add an "insert_first"
-function than just use "append"?"""
+"""
 
 class Element(object):
     def __init__(self, value):
