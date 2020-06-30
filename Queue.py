57e18a3d985d9e91884ce66b0425a30375baa316
Jane Yang <50490168+SavannahY@users.noreply.github.com>
2020-06-30 11:08:10 -0400

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -1,8 +1,7 @@
 """Make a Queue class using a list!
 Hint: You can use any Python list method
 you'd like! Try to write each one in as 
-few lines as possible.
-Make sure you pass the test cases too!"""
+few lines as possible."""
 
 class Queue:
     def __init__(self, head=None):
